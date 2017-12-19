e69298f442c5fe89b87064a4e076d7ed3cecceb5
hirony-jp <dev.hirony@gmail.com>
2017-12-20 00:12:32 +0900

    add navbar.

--- header.php
+++ header.php
@@ -9,3 +9,23 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
   </head>
   <body>
+  <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="/index.php">VideoMan</a>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="#">hoge</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">fuga</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">piyo</a>
+        </li>
+        </ul>
+    </div>
+    </nav>
