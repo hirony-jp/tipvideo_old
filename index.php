85de8e54e0ca49374eb0c67458361c56d2c1c6ff
hirony-jp <dev.hirony@gmail.com>
2017-12-20 23:01:06 +0900

    add explanation. on index.php

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php require("header.php"); ?>
 
-
-<h2>Videoman</h2>
-<p>お気に入りの動画に投げゼニ！！</p>
+<section class="jumbotron text-center">
+    <div class="container">
+    <h1 class="jumbotron-heading">Videoman</h1>
+    <p class="lead text-muted">動画を投稿して投げ銭をもらおう！お気に入りの動画に投げ銭しよう！</p>
+    <!-- <p>
+        <a href="#" class="btn btn-primary">Main call to action</a>
+        <a href="#" class="btn btn-secondary">Secondary action</a>
+    </p> -->
+    </div>
+</section>
 
 <!-- 投稿フォーム -->
 <!-- 投稿された動画群を表示 -->
