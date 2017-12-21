8f19d0d6b286abebc13fe2a204bd5c584a7b4ad9
hirony-jp <dev.hirony@gmail.com>
2017-12-22 01:03:21 +0900

    change maxdata

--- view/list.php
+++ view/list.php
@@ -6,10 +6,9 @@
                     <iframe width="100%" height="315" src="https://www.youtube.com/embed/BOfg1w2vY64" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 ';
-    $maxdata = 5;
+    $maxdata = 10;
 
-    // ３個ずつ表示する
-    for($j=0; $j<3; $j++){
+    for($j=0; $j<$maxdata; $j++){
         print($div_class_card);
         print($videodata);
         print($div_end);
