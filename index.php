4ee794e673101db1d4cb8924fbd53ddf21af38c1
hirony-jp <dev.hirony@gmail.com>
2017-12-22 01:06:49 +0900

    change posting url

--- index.php
+++ index.php
@@ -5,14 +5,12 @@
     <h1 class="jumbotron-heading">Videoman</h1>
     <p class="lead text-muted">動画を投稿して投げ銭をもらおう！お気に入りの動画に投げ銭しよう！</p>
     <p>
-        <a href="/view/help.php" class="btn btn-primary">ヘルプ</a>
-        <a href="/view/fund.php" class="btn btn-primary">募金</a>
+        <a href="view/help.php" class="btn btn-primary">ヘルプ</a>
+        <a href="view/post.php" class="btn btn-primary">投稿する</a>
     </p>
     </div>
 </section>
 
-<!-- 投稿フォーム -->
-<?php require("view/post.php"); ?>
 
 <!-- 投稿された動画群を表示 -->
 <div class="container">
