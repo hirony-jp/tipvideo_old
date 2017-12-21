faba96a397959339f1b2a5b5bcab5d018110d781
hirony-jp <dev.hirony@gmail.com>
2017-12-21 19:05:46 +0900

    add posting form on index.php

--- index.php
+++ index.php
@@ -5,14 +5,34 @@
     <h1 class="jumbotron-heading">Videoman</h1>
     <p class="lead text-muted">動画を投稿して投げ銭をもらおう！お気に入りの動画に投げ銭しよう！</p>
     <p>
-        <a href="#" class="btn btn-primary">ヘルプ</a>
-        <a href="#" class="btn btn-primary">募金</a>
+        <a href="/view/help.php" class="btn btn-primary">ヘルプ</a>
+        <a href="/view/fund.php" class="btn btn-primary">募金</a>
     </p>
     </div>
 </section>
 
 <!-- 投稿フォーム -->
-
+<div class="card">
+    <div class="card-block">
+        <form action="" method="post">
+            動画タイトルを入力してください。<br>
+            <div class="input-group">
+            <span class="input-group-addon" id="basic-addon1">動画タイトル</span>
+            <input type="text" class="form-control" placeholder="Input Your Video Title" aria-describedby="basic-addon1">
+            </div>
+            動画のURLを入力してください。<br>
+            <div class="input-group">
+            <span class="input-group-addon" id="basic-addon2">動画URL</span>
+            <input type="text" class="form-control" placeholder="Input Your Video URL" aria-describedby="basic-addon1">
+            </div>
+            自分のTwitter IDを入力してください。<br>
+            <div class="input-group">
+            <span class="input-group-addon" id="basic-addon3">自分のTwitterID</span>
+            <input type="text" class="form-control" placeholder="Input Your Twitter ID" aria-describedby="basic-addon1">
+            </div>
+        </form>
+    </div>
+</div>
 <!-- 投稿された動画群を表示 -->
 
 
