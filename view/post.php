96fea1287da3ace06bbeac4ea5e2eb0c204c24a6
hirony-jp <dev.hirony@gmail.com>
2017-12-21 18:10:45 +0900

    add form on post.php

---
+++ view/post.php
@@ -0,0 +1,9 @@
+<div class="input-group">
+    <span class="input-group-addon" id="basic-addon1">@</span>
+    <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+    <span class="input-group-addon" id="basic-addon1">$</span>
+    <input type="text" class="form-control" placeholder="posting" aria-describedby="basic-addon1">
+    <span class="input-group-addon" id="basic-addon1">#</span>
+    <input type="text" class="form-control" placeholder="sending" aria-describedby="basic-addon1">
+    
+</div>
