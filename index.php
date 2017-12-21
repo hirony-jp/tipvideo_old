3b42388b5516f622cee630acf13671fc688da09e
hirony-jp <dev.hirony@gmail.com>
2017-12-22 01:06:05 +0900

    cutting postform

--- index.php
+++ index.php
@@ -12,32 +12,8 @@
 </section>
 
 <!-- 投稿フォーム -->
-<div class="card">
-    <div class="card-block">
-        <form action="" method="post">
-            自分のTwitter IDを入力してください。<br>
-            <div class="input-group">
-            <span class="input-group-addon" id="basic-addon1">自分のTwitterID</span>
-            <input type="text" class="form-control" placeholder="Input Your Twitter ID" aria-describedby="basic-addon1">
-            </div>
-            動画タイトルを入力してください。<br>
-            <div class="input-group">
-            <span class="input-group-addon" id="basic-addon1">動画タイトル</span>
-            <input type="text" class="form-control" placeholder="Input Your Video Title" aria-describedby="basic-addon1">
-            </div>
-            動画のURLを入力してください。<br>
-            <div class="input-group">
-            <span class="input-group-addon" id="basic-addon1">動画URL</span>
-            <input type="text" class="form-control" placeholder="Input Your Video URL" aria-describedby="basic-addon1">
-            </div>
-            動画説明文を入力してください。<br>
-            <div class="input-group">
-            <span class="input-group-addon" id="basic-addon1">動画説明文</span>
-            <input type="text" class="form-control" placeholder="Input Your Video description" aria-describedby="basic-addon1">
-            </div>
-        </form>
-    </div>
-</div>
+<?php require("view/post.php"); ?>
+
 <!-- 投稿された動画群を表示 -->
 <div class="container">
     <div class="row">
---
+++ view/post.php
@@ -0,0 +1,26 @@
+<div class="card">
+    <div class="card-block">
+        <form action="" method="post">
+            自分のTwitter IDを入力してください。<br>
+            <div class="input-group">
+            <span class="input-group-addon" id="basic-addon1">自分のTwitterID</span>
+            <input type="text" class="form-control" placeholder="Input Your Twitter ID" aria-describedby="basic-addon1">
+            </div>
+            動画タイトルを入力してください。<br>
+            <div class="input-group">
+            <span class="input-group-addon" id="basic-addon1">動画タイトル</span>
+            <input type="text" class="form-control" placeholder="Input Your Video Title" aria-describedby="basic-addon1">
+            </div>
+            動画のURLを入力してください。<br>
+            <div class="input-group">
+            <span class="input-group-addon" id="basic-addon1">動画URL</span>
+            <input type="text" class="form-control" placeholder="Input Your Video URL" aria-describedby="basic-addon1">
+            </div>
+            動画説明文を入力してください。<br>
+            <div class="input-group">
+            <span class="input-group-addon" id="basic-addon1">動画説明文</span>
+            <input type="text" class="form-control" placeholder="Input Your Video description" aria-describedby="basic-addon1">
+            </div>
+        </form>
+    </div>
+</div>
