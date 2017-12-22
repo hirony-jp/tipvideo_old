292e6f5e41c32353640fc77aea1001c320e1cab3
hirony-jp <dev.hirony@gmail.com>
2017-12-22 18:05:53 +0900

    add name on post.php

--- view/post.php
+++ view/post.php
@@ -3,26 +3,26 @@
 <div class="container">
     <div class="card">
         <div class="card-block">
-            <form action="" method="post">
+            <form action="../server/insert.php" method="post">
                 自分のTwitter IDを入力してください。<br>
                 <div class="input-group">
                 <span class="input-group-addon" id="basic-addon1">自分のTwitterID</span>
-                <input type="text" class="form-control" placeholder="Input Your Twitter ID" aria-describedby="basic-addon1">
+                <input type="text" name="twid" class="form-control" placeholder="Input Your Twitter ID" aria-describedby="basic-addon1">
                 </div>
                 動画タイトルを入力してください。<br>
                 <div class="input-group">
-                <span class="input-group-addon" id="basic-addon1">動画タイトル</span>
-                <input type="text" class="form-control" placeholder="Input Your Video Title" aria-describedby="basic-addon1">
+                <span class="input-group-addon" id="basic-addon2">動画タイトル</span>
+                <input type="text" name="vtitle"class="form-control" placeholder="Input Your Video Title" aria-describedby="basic-addon2">
                 </div>
                 動画のURLを入力してください。<br>
                 <div class="input-group">
-                <span class="input-group-addon" id="basic-addon1">動画URL</span>
-                <input type="text" class="form-control" placeholder="Input Your Video URL" aria-describedby="basic-addon1">
+                <span class="input-group-addon" id="basic-addon3">動画URL</span>
+                <input type="text" name="vurl" class="form-control" placeholder="Input Your Video URL" aria-describedby="basic-addon3">
                 </div>
                 動画説明文を入力してください。<br>
                 <div class="input-group">
-                <span class="input-group-addon" id="basic-addon1">動画説明文</span>
-                <input type="text" class="form-control" placeholder="Input Your Video description" aria-describedby="basic-addon1">
+                <span class="input-group-addon" id="basic-addon4">動画説明文</span>
+                <input type="text" name="vdesc" class="form-control" placeholder="Input Your Video description" aria-describedby="basic-addon4">
                 </div>
             </form>
         </div>
