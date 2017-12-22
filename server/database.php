e99ce6f4c31a091b11d1f99d3ba408a7f7e8d5b5
hirony-jp <dev.hirony@gmail.com>
2017-12-22 13:58:10 +0900

    add define and connection

--- server/database.php
+++ server/database.php
@@ -1,6 +1,13 @@
 <?php
-    define(DB_HOST, "localhost");
-    define(DB_USER, "root");
-    define(DB_PASS, "password");
-    define(DB_NAME, "videoman");
+    define("DB_HOST", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASS", "password");
+    define("DB_NAME", "videoman");
+    // データベース接続コネクションの生成
+    try {
+        $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8',DB_USER,DB_PASS,
+        array(PDO::ATTR_EMULATE_PREPARES => false));
+    } catch (PDOException $e) {
+        exit('データベース接続失敗。'.$e->getMessage());
+    }
 ?>
