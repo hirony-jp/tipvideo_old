0999d1c032463243f4b762d03c77861182f27e7f
hirony-jp <dev.hirony@gmail.com>
2018-01-20 22:08:33 +0900

    change help message

--- view/help.php
+++ view/help.php
@@ -6,9 +6,9 @@
 <hr><h2>Step:1</h2><p>Youtubeで埋め込みコードをコピーします。</p><img class="helpimg" src="help001.png" width="75%"><br>
 <hr><h2>Step:2</h2><img class="helpimg" src="help002.png" width="75%"><br>
 <hr><h2>Step:3</h2><img class="helpimg" src="help003.png" width="75%"><br>
-<hr><h2>Step:4</h2><img class="helpimg" src="help004.png" width="75%"><br>
-</center>
+<hr><h2>Step:4</h2><p>必要事項を記入します。</p><img class="helpimg" src="help004.png" width="75%"><br>
 <br>
 <p><strong>確認ボタンをクリックし入力内容を確認して問題なければ投稿しよう！！</strong></p>
 <hr>
 <br>
+</center>
