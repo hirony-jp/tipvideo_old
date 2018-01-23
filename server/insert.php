10d41040557fcc87960b9074dc3f7f4f8bb084f2
hirony-jp <dev.hirony@gmail.com>
2018-01-23 11:33:57 +0900

    add isset check in select,insert.php

--- server/insert.php
+++ server/insert.php
@@ -1,36 +1,40 @@
 <?php
-    require("database.php");
+    if ( !isset($_POST["twid"]) || !isset($_POST["vtitle"]) || !isset($_POST["vurl"]) || !isset($_POST["vdesc"]) ) {
+        header("Location: index.php");
+    } else {
 
-    // SQLの生成
-    $insert_sql = 'insert into Posts(postid, postdate, twitterid, videotitle, videourl, videodescription, tipcount) value(0, now(), ?, ?, ?, ?, 0);';
+        require("database.php");
 
-    // POSTされたデータを取得する。
-    $tw_id = $_POST["twid"];
-    $v_title = $_POST["vtitle"];
-    $v_url = $_POST["vurl"];
-    $v_description = $_POST["vdesc"];
-    $tw_id = htmlspecialchars($tw_id, ENT_QUOTES);
-    $v_title = htmlspecialchars($v_title, ENT_QUOTES);
-    $v_url = htmlspecialchars($v_url, ENT_QUOTES);
-    $v_description = htmlspecialchars($v_description, ENT_QUOTES);
-    $cut = 32;
-    $cut_back = -19;
-    $addiframetext = '<iframe width="100%"';
-    $addiframetext_back = '></iframe>';
+        // SQLの生成
+        $insert_sql = 'insert into Posts(postid, postdate, twitterid, videotitle, videourl, videodescription, tipcount) value(0, now(), ?, ?, ?, ?, 0);';
 
-    $replace = substr( $v_url, $cut, strlen($v_url)-$cut);
-    $replace = substr( $replace, 0, $cut_back);
-    $new_v_url = $addiframetext.$replace.$addiframetext_back;
+        // POSTされたデータを取得する。
+        $tw_id = $_POST["twid"];
+        $v_title = $_POST["vtitle"];
+        $v_url = $_POST["vurl"];
+        $v_description = $_POST["vdesc"];
+        $tw_id = htmlspecialchars($tw_id, ENT_QUOTES);
+        $v_title = htmlspecialchars($v_title, ENT_QUOTES);
+        $v_url = htmlspecialchars($v_url, ENT_QUOTES);
+        $v_description = htmlspecialchars($v_description, ENT_QUOTES);
+        $cut = 32;
+        $cut_back = -19;
+        $addiframetext = '<iframe width="100%"';
+        $addiframetext_back = '></iframe>';
 
-    // テーブルにデータを挿入する。
-    $stmt = $pdo -> prepare($insert_sql);
-    $stmt->bindParam(1, $tw_id, PDO::PARAM_STR);
-    $stmt->bindParam(2, $v_title, PDO::PARAM_STR);
-    $stmt->bindParam(3, $new_v_url, PDO::PARAM_STR);
-    $stmt->bindParam(4, $v_description, PDO::PARAM_STR);
+        $replace = substr( $v_url, $cut, strlen($v_url)-$cut);
+        $replace = substr( $replace, 0, $cut_back);
+        $new_v_url = $addiframetext.$replace.$addiframetext_back;
 
-    $stmt->execute();
+        // テーブルにデータを挿入する。
+        $stmt = $pdo -> prepare($insert_sql);
+        $stmt->bindParam(1, $tw_id, PDO::PARAM_STR);
+        $stmt->bindParam(2, $v_title, PDO::PARAM_STR);
+        $stmt->bindParam(3, $new_v_url, PDO::PARAM_STR);
+        $stmt->bindParam(4, $v_description, PDO::PARAM_STR);
 
-    header( "Location: ../index.php" ) ;
+        $stmt->execute();
 
+        header( "Location: ../index.php" ) ;
+    }
 ?>
--- server/select.php
+++ server/select.php
@@ -1,28 +1,32 @@
 <?php
-    require("database.php");
+    if ( !isset($_GET["page"]) ){
+        header("Location: index.php");
+    } else {
 
-    $page = $_GET["page"] - 9;
-    $page = $page + 9;
-    $select_sql = 'select * from Posts order by postdate desc limit 9 offset '.$page.';';
+        require("database.php");
+
+        $page = $_GET["page"] - 9;
+        $page = $page + 9;
+        $select_sql = 'select * from Posts order by postdate desc limit 9 offset '.$page.';';
 
 
-    // cardを表示する
-    $stmt = $pdo->query($select_sql);
-    while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
-        $postid = $row["postid"];
-        $postdate = $row["postdate"];
-        $twitterid = $row["twitterid"];
-        $videotitle = $row["videotitle"];
-        $videourl = $row["videourl"];
-        $videodescription = $row["videodescription"];
-        $div_class_card = '<div class="col-lg-4 col-sm-6 portfolio-item" style="display:-webkit-box;display:-webkit-flex; display:-ms-flexbox; display: flex;"><div class="card"><div class="card-body">';
-        $div_end = '</div>';
-        $videodata = $videourl.'<h4 class="card-title">'.$videotitle.'</h4><p class="card-text">'.$videodescription.'</p><a href="#" class="btn btn-primary btn-block">tip</a>';
-        print($div_class_card);
-        print($videodata);
-        print($div_end);
-        print($div_end);
-        print($div_end);
+        // cardを表示する
+        $stmt = $pdo->query($select_sql);
+        while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
+            $postid = $row["postid"];
+            $postdate = $row["postdate"];
+            $twitterid = $row["twitterid"];
+            $videotitle = $row["videotitle"];
+            $videourl = $row["videourl"];
+            $videodescription = $row["videodescription"];
+            $div_class_card = '<div class="col-lg-4 col-sm-6 portfolio-item" style="display:-webkit-box;display:-webkit-flex; display:-ms-flexbox; display: flex;"><div class="card"><div class="card-body">';
+            $div_end = '</div>';
+            $videodata = $videourl.'<h4 class="card-title">'.$videotitle.'</h4><p class="card-text">'.$videodescription.'</p><a href="#" class="btn btn-primary btn-block">tip</a>';
+            print($div_class_card);
+            print($videodata);
+            print($div_end);
+            print($div_end);
+            print($div_end);
+        }
     }
-
 ?>
